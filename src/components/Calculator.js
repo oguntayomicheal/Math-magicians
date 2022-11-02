9000aa8c43bce7d5dc3f7963a096f22ea57b9d7b
Micheal <oguntayomicheal@gmail.com>
2022-11-02 23:17:33 +0100

    Add logic to the application

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,60 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
+  buttonClick = (buttonName, obj = this.state) => {
+    const result = calculate(obj, buttonName);
+    this.setState(result);
+  };
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="container">
-        <div className="result">0</div>
+        <div className="result">
+          <span>{total}</span>
+          <span>{operation}</span>
+          <span>{next}</span>
+        </div>
         <div className="buttons">
           <div className="row1 row">
-            <button type="button" className="btnWidth"> AC </button>
-            <button type="button" className="btnWidth"> +/- </button>
-            <button type="button" className="btnWidth"> % </button>
-            <button type="button" className="btnWidth bgd-color2"> ÷ </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('AC')}> AC </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('+/-')}> +/- </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('%')}> % </button>
+            <button type="button" className="btnWidth bgd-color2" onClick={() => this.buttonClick('÷')}> ÷ </button>
           </div>
           <div className="row2 row">
-            <button type="button" className="btnWidth"> 7 </button>
-            <button type="button" className="btnWidth"> 8 </button>
-            <button type="button" className="btnWidth"> 9 </button>
-            <button type="button" className="btnWidth bgd-color2"> x </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('7')}> 7 </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('8')}> 8 </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('9')}> 9 </button>
+            <button type="button" className="btnWidth bgd-color2" onClick={() => this.buttonClick('x')}> x </button>
           </div>
           <div className="row3 row">
-            <button type="button" className="btnWidth"> 4 </button>
-            <button type="button" className="btnWidth"> 5 </button>
-            <button type="button" className="btnWidth "> 6 </button>
-            <button type="button" className="btnWidth bgd-color2"> - </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('4')}> 4 </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('5')}> 5 </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('6')}> 6 </button>
+            <button type="button" className="btnWidth bgd-color2" onClick={() => this.buttonClick('-')}> - </button>
           </div>
           <div className="row4 row">
-            <button type="button" className="btnWidth"> 1 </button>
-            <button type="button" className="btnWidth"> 2 </button>
-            <button type="button" className="btnWidth"> 3 </button>
-            <button type="button" className="btnWidth bgd-color2"> + </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('1')}> 1 </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('2')}> 2 </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('3')}> 3 </button>
+            <button type="button" className="btnWidth bgd-color2" onClick={() => this.buttonClick('+')}> + </button>
           </div>
           <div className="row5 row">
-            <button type="button" className="btnWidth50"> 0 </button>
-            <button type="button" className="btnWidth"> . </button>
-            <button type="button" className="btnWidth bgd-color2"> = </button>
+            <button type="button" className="btnWidth50" onClick={() => this.buttonClick('0')}> 0 </button>
+            <button type="button" className="btnWidth" onClick={() => this.buttonClick('.')}> . </button>
+            <button type="button" className="btnWidth bgd-color2" onClick={() => this.buttonClick('=')}> = </button>
           </div>
         </div>
       </div>
